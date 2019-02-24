5ace25cda8f26d4649e36ab29ebfb97a1881db64
Баранов Андрей Николаевич <Baranov_AN@st.by>
2019-02-24 19:35:13 +0300

    Added gulp-rev-dist-clean for deleting original resource files after versioning.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 const gulp = require('gulp');
 const rev = require('gulp-rev');
-const revDel = require('rev-del');
 const collect = require('gulp-rev-collector');
- 
+const revDistClean = require('gulp-rev-dist-clean');
+
 gulp.task('default', () =>
     gulp.src('src/**/*.css')
         .pipe(rev())
         .pipe(gulp.dest('dist'))
 );
 
-// gulp.task('copy-data', function() {
-//     return gulp.src(["./src/**/*.*"])
-//       .pipe(gulp.dest('dist'));
-// });
-
 gulp.task('copy-data', function() {
-    return gulp.src(["./src/**/*.*", "!./src/**/*.css", "!./src/**/*.js", "!./src/**/*.map"])
+    return gulp.src(["./src/**/*.*"])
       .pipe(gulp.dest('dist'));
 });
 
@@ -24,15 +19,23 @@ gulp.task('revision:rename', () =>
             "src/**/*.js"])
   .pipe(rev())
   .pipe(gulp.dest('dist'))
-  .pipe(rev.manifest('manifest.json'))
-  .pipe(revDel({ dest: 'dist' }))
+  .pipe(rev.manifest())
   .pipe(gulp.dest('dist'))
 );
 
-gulp.task('revision:updateReferences', () =>
-   gulp.src(['dist/manifest.json','dist/**/*.html'])
+gulp.task('revision:update-references', () =>
+   gulp.src(['dist/rev-manifest.json','dist/**/*.html'])
    .pipe(collect())
    .pipe(gulp.dest('dist'))
 );
 
-gulp.task('compile:production', gulp.series('copy-data', 'revision:rename', 'revision:updateReferences'));
+gulp.task('revision:rev-dist-clean', () =>
+    gulp.src(["dist/**/*.css", "dist/**/*.js"])
+      .pipe(revDistClean('dist/rev-manifest.json', {
+        keepRenamedFiles: true,
+        keepOriginalFiles: false
+      })
+    )
+);
+
+gulp.task('compile:production', gulp.series('copy-data', 'revision:rename', 'revision:update-references', 'revision:rev-dist-clean'));
